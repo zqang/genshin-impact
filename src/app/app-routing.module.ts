be26efadf90bf3e84644aa12c4b9967578432fe8
Zqang <alvinang1406@gmail.com>
2021-06-29 13:39:11 +0800

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '/', component: AppComponent},
+  {path: 'characters', component: CharactersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
